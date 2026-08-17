0ebb679f7f1eff62c5eea7b17c90dae8e9e00cb4
marcinmoskala <marcinmoskala@gmail.com>
2026-08-17 09:28:40 +0200

    Init

--- src/engine/GameState.kt
+++ src/engine/GameState.kt
@@ -1,4 +1,21 @@
 package engine
 
-class Game {
+class GameState
+
+enum class Action {
+    BuildFarm,
+    SpawnWarrior,
+    // ...
+}
+
+class UserAction(
+    val action: Action,
+    val position: Position,
+)
+
+data class Position(val x: Int, val y: Int)
+
+enum class Tile {
+    Map, 
+    Unit,
 }
--- test/engine/GameStateTest.kt
+++ test/engine/GameStateTest.kt
@@ -1,4 +1,50 @@
+package engine
+
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
 class GameStateTest {
-  
+    // Sample game state with a map
+    val state = GameState(
+        // ...
+    )
+    
+    @Test
+    fun `gives correct unit actions`() {
+        val actions: List<UserAction> = state.getUserActions(
+            Tile.Unit,
+            unitPosition,
+        )
+        
+        val expectedActions = listOf<UserAction>(
+            // ...
+        )
+        
+        assertEquals(expectedActions, actions)
+    }
+    
+    @Test
+    fun `gives correct map actions`() {
+        val actions: List<UserAction> = state.getUserActions(
+            Tile.Map,
+            unitPosition,
+        )
+        
+        val expectedActions = listOf<UserAction>(
+            // ...
+        )
+        
+        assertEquals(expectedActions, actions)
+    }
+    
+    @Test
+    fun `gives correct user actions`() {
+        val actions: List<UserAction> = state.getUserActions()
+        
+        val expectedActions = listOf<UserAction>(
+            // ...
+        )
+        
+        assertEquals(expectedActions, actions)
+    }
 }
